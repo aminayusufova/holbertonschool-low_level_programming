6c433c1bfba86a0b1359dabc71f4b5e863004096
aminayusufova <yusufovamina25@gmail.com>
2024-02-19 18:58:53 +0000

    Commited

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = 0; num <= 9; num++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
