8fbd0f043f34e81abdb00a72e66e7885eddc572b
aminayusufova <yusufovamina25@gmail.com>
2024-02-20 05:33:56 +0000

    commited

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -6,10 +6,13 @@
   *
   * Return: 1 for lowercase character or 0 for anything else
   */
-int _islower(int c){
+int _islower(int c)
+{
 
-	if(c<'z' && c>'a'){
-	return (1);
+	if(c < 'z' && c > 'a')
+	{
+		return (1);
 	}
+
 	else return (0);
 }
