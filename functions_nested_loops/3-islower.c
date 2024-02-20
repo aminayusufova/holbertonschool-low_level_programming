80461796fc352db520dcf08209c6423075664b5c
aminayusufova <yusufovamina25@gmail.com>
2024-02-20 05:31:52 +0000

    commited

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+  * _islower - Checks for lowercase character
+  * @c: The character to be checked
+  *
+  * Return: 1 for lowercase character or 0 for anything else
+  */
 int _islower(int c){
 
 	if(c<'z' && c>'a'){
