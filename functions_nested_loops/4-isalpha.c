72cc55839be7ab55a8f5b2259ddddd9e8234b793
aminayusufova <yusufovamina25@gmail.com>
2024-02-20 05:41:41 +0000

    commited

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+  * _isalpha - Checks for alphabetic character
+  * @c: The character to be checked
+  *
+  * Return: 1 for alphabetic character or 0 for anything else
+  */
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
