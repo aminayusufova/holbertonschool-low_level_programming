08a14c987f55671ca83584be37d40742d6d45e61
aminayusufova <yusufovamina25@gmail.com>
2024-02-24 19:17:19 +0000

    commited

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -14,8 +14,9 @@ void print_times_table(int n)
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-			for (j = 1; j <= n; j++){
-				s=i*j;
+			for (j = 1; j <= n; j++)
+			{
+				s = i * j;
 				putformat(s);
 			}	
 			_putchar('\n');
